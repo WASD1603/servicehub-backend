4a79212449d045ca6c7da32cc2441944a371d3b5
Pranav <pranavganesh1632004@gmail.com>
2026-05-04 21:34:46 +0530

    fix model import case

--- routes/booking.js
+++ routes/booking.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Booking = require("../models/Booking");
+const Booking = require("../models/booking");
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
